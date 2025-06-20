8cbfdfc9c1a7000b4c776434802e5631399dc7fd
edgar-fs24sul <edgarfs24sul@gmail.com>
2025-06-20 10:51:32 -0300

    inclusão da localização

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Função do relógio
 function updateClock(){
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
@@ -8,3 +9,42 @@ function updateClock(){
 
 setInterval(updateClock, 1000);
 updateClock();
+
+// Função Localização
+function showLocation() {
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        // Chamada para API do OpenCage
+        const apiKey = 'e4cc69c63cd84cecb0a668e768e90ea4';
+        const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=${apiKey}&language=pt`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+            if (data.results.length > 0) {
+                const location = data.results[0].components;
+                const cidade = location.city || location.town || location.village || 'Cidade não encontrada';
+                const pais = location.country || 'País não encontrado';
+                document.getElementById('location').textContent = `Você está em: ${cidade}, ${pais}`;
+            } else {
+                document.getElementById('location').textContent = "Localização não encontrada.";
+            }
+            })
+            .catch(error => {
+            document.getElementById('location').textContent = "Erro ao buscar cidade.";
+            });
+        },
+        function(error) {
+        document.getElementById('location').textContent = `Erro ao obter localização: ${error.message}`;
+        }
+    );
+    } else {
+    document.getElementById('location').textContent = "Geolocalização não suportada.";
+    }
+}
+
+showLocation();
